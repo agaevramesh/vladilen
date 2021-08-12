e49504982656e2b606281839ed638faaeb66b48b
agaevramesh <55577563+agaevramesh@users.noreply.github.com>
2021-08-12 14:10:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const modal = $.modal({
+    title: 'Vladilen Modal',
+    closable: true,
+    content: `
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '200px'
+})
